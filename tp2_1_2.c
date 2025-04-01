3533fa8e1e090009c1d793a5e81f9bc9432006a6
SrCh1n0 <facundo.lopez_hd@hotmail.com>
2025-04-01 15:31:33 -0300

    Agrego las respuestas al .txt y correccion del archivo tp2_1_2.c

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -8,15 +8,10 @@ int main(){
     int i;
     double vt[N], *pVT;
     srand (time(NULL));
-    for(i = 0; i < N; i++)
-    {
-        vt[i] = 1 + rand()%100;
-        printf("%f\n", vt[i]);
-    }
-    printf("Ahora recorro el arreglo con aritmetica de punteros: \n");
     pVT = vt;
     for(i = 0; i < N; i++)
     {
+        *pVT = 1 + rand()%100;
         printf("%f\n", *pVT++);
     }
     return 0;
